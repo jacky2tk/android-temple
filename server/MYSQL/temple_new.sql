21922e6036d97e5a97dce0230c0cc25488006e61
jacky2tk@gmail.com <jacky2tk@gmail.com@d25c00ba-8ae0-33fb-a64b-def8d6298f36>
2012-10-02 17:22:46 +0000

    Server: 1. 更新空白資料庫結構, 使全部改成 Not Null (不能空值) 2. temple.sql 增加全台寺廟資料

--- server/MYSQL/temple_new.sql
+++ server/MYSQL/temple_new.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- 主機: localhost
--- 建立日期: Sep 28, 2012, 08:07 AM
+-- 建立日期: Oct 02, 2012, 05:03 PM
 -- 伺服器版本: 5.0.51
 -- PHP 版本: 5.2.6
 
@@ -21,10 +21,15 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE `temple` (
   `m_id` int(11) NOT NULL auto_increment,
-  `m_name` varchar(20) default NULL,
-  `m_god` varchar(20) default NULL,
-  `m_address` varchar(80) default NULL,
-  `m_lat` float default NULL,
-  `m_lng` float default NULL,
+  `m_name` varchar(20) NOT NULL,
+  `m_god` varchar(20) NOT NULL,
+  `m_address` varchar(80) NOT NULL,
+  `m_lat` float NOT NULL,
+  `m_lng` float NOT NULL,
   PRIMARY KEY  (`m_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=695 ;
+
+-- 
+-- 列出以下資料庫的數據： `temple`
+-- 
+
